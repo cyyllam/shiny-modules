58062f9a631ea3a65ea6475d6f8649bdd12b17bf
cyyllam <cyyllam@gmail.com>
2022-10-06 22:00:01 -0700

    added comments to template

--- modules/module-template.R
+++ modules/module-template.R
@@ -1,10 +1,15 @@
 # Modules contains a UI and Server component. 
 # Call ui component in ui.R; call server component in server.R
+# Add additional arguments to functions if necessary
 
 module_ui <- function(id) {
   ns <- NS(id) # ns is namespace
   
-  tagList( # use tagList with a set of UI widgets; wrap every ID in widget with ns()
+  tagList( 
+    # use tagList with a set of UI widgets; wrap every ID in widget with ns()
+    # selectInput(ns('dropdown'),
+    #             label = 'xxxx',
+    #             choices = 'xxxx')
     
   )
   
@@ -12,7 +17,9 @@ module_ui <- function(id) {
 
 module_server <- function(id) {
   
-  moduleServer(id, function(input, output, session) { # every server component will have a moduleServer, everything will be nested inside here
+  moduleServer(id, function(input, output, session) { 
+    # every server component will have a moduleServer, everything will be nested inside here
+    # reference inputs in the ui component like usual (input$dropdown)
 
     
   }) # end moduleServer
