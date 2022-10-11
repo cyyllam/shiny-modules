45fe748e58724e2e2787d14299d3a509fe33e333
cyyllam <cyyllam@gmail.com>
2022-10-11 11:38:42 -0700

    added triggers for tab three

--- server.R
+++ server.R
@@ -4,9 +4,13 @@ server <- function(input, output, session) {
   scatterplot_server('plot1', mtcars)
   scatterplot_server('plot2', diamonds)
   
-  # calling server component of modules AND using another modules' information as arguments 
-  # Notice the backticks and the hyphenated combo of id and parameter name
-  text_server('text1', 'Tab One', input$`plot1-x`, input$`plot1-y`)
-  text_server('text2', 'Tab Two', input$`plot2-x`, input$`plot2-y`)
+  observeEvent(input$`plot1-go` | input$`plot2-go`, {
+    # calling server component of modules AND using another modules' information as arguments 
+    # Notice the backticks and the hyphenated combo of id and parameter name
+    
+    text_server('text1', 'Tab One', input$`plot1-x`, input$`plot1-y`)
+    text_server('text2', 'Tab Two', input$`plot2-x`, input$`plot2-y`)
+  })
+  
   
 }
